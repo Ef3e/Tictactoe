f92329671189d79de1429aaf23d9ed8589980074
Efe <efekocak070@gmail.com>
2023-02-24 16:14:15 +0300

    Update pTicTacToe.py

--- pTicTacToe.py
+++ pTicTacToe.py
@@ -1,14 +1,7 @@
 import random
 import os
-
 sayilar = []
 cizgiler = []
-"""
-0   1  2
-3   4  5
-6   7  8
-
-"""
 def ekle(dene = ""):
     if dene == "sil":
         cizgiler.clear()
@@ -19,16 +12,13 @@ def ekle(dene = ""):
 def yazdir_cizgiler():
     ekle()
     return f"{cizgiler[0:3]}   {sayilar[0:3]}\n{cizgiler[3:6]}   {sayilar[3:6]}\n{cizgiler[6:9]}   {sayilar[6:9]}"
-
 os.system("cls")
 print(yazdir_cizgiler())
-
 class tictactoe:
-    def yatay_kontrol(self,arg):
+    def yatay_kontrol(arg):
         if (cizgiler[0] == arg and cizgiler[1] == arg and cizgiler[2] == arg) or (cizgiler[3] == arg and cizgiler[4] == arg and cizgiler[5] == arg) or (cizgiler[6] == arg and cizgiler[7] == arg and cizgiler[8] == arg) or(cizgiler[0] == arg and cizgiler[3] == arg and cizgiler[6] == arg) or(cizgiler[1] == arg and cizgiler[4] == arg and cizgiler[7] == arg) or(cizgiler[2] == arg and cizgiler[5] == arg and cizgiler[8] == arg) or(cizgiler[0] == arg and cizgiler[4] == arg and cizgiler[8] == arg) or(cizgiler[2] == arg and cizgiler[4] == arg and cizgiler[6] == arg):
-            print(f"{arg} Oyuncusu kazandi ")
-            exit()
-    def oyuncu(self):
+            return True
+    def __init__(self):
         self.ilk_oyuncu = input("ilk oyuncunun isimi = ") #oyuncularin isimini aldik
         self.ikinci_oyuncu = input("ikinci oyuncunun isimi = ")  
         self.oyuncular = [self.ikinci_oyuncu,self.ilk_oyuncu]    
@@ -36,22 +26,55 @@ class tictactoe:
         self.oyuncular.remove(self.x_oyuncu)
         self.o_oyuncu = self.oyuncular[0] 
         self.dogru = True
+    def oyun(self):
         while self.dogru:
-            self.x_oyuncu_hamle = int(input(f"{self.x_oyuncu}(x) yer sec = "))
+            dogru = True
+            while dogru:
+                try:
+                    self.x_oyuncu_hamle = int(input(f"{self.x_oyuncu}(x) yer sec = "))
+                    dogru = False
+                except:
+                    dogru = True
             while cizgiler[self.x_oyuncu_hamle-1] != "-":
                 self.x_oyuncu_hamle = int(input(f"{self.x_oyuncu} bos bir yer sec = "))
+                
             cizgiler[self.x_oyuncu_hamle-1] = "X"
             sayilar[self.x_oyuncu_hamle-1] = 0
             os.system("cls")
             print(yazdir_cizgiler())
-            tictactoe().yatay_kontrol("X")
-            self.o_oyuncu_hamle = int(input(f"{self.o_oyuncu}(O) yer secsin = "))
+            kontrol = tictactoe.yatay_kontrol("X")
+            if kontrol == True:
+                print(f"{self.o_oyuncu} oyunu kazandı")
+                evet = input("bir daha oynamak istermisiniz = ").lower()
+                if evet[0] == "e":
+                    ekle("sil")
+                    os.system("cls")
+                    print(yazdir_cizgiler())
+                    tictactoe()
+                else:
+                    exit()
+            dogru = True
+            while dogru:
+                try:
+                    self.o_oyuncu_hamle = int(input(f"{self.o_oyuncu}(O) yer secsin = "))
+                    dogru = False
+                except:
+                    dogru = True
             while cizgiler[self.o_oyuncu_hamle-1] != "-":
                 self.o_oyuncu_hamle = int(input(f"{self.o_oyuncu} bos bir yer sec = "))
             cizgiler[self.o_oyuncu_hamle-1] = "O"
             sayilar[self.o_oyuncu_hamle-1] = 0
             os.system("cls")
             print(yazdir_cizgiler())
-            tictactoe().yatay_kontrol("O")
-
-tictactoe().oyuncu()
+            kontrol = tictactoe.yatay_kontrol("O")
+            if kontrol == True:
+                print(f"{self.o_oyuncu} oyunu kazandı")
+                evet = input("bir daha oynamak istermisiniz = ").lower()
+                if evet[0] == "e":
+                    ekle("sil")
+                    os.system("cls")
+                    print(yazdir_cizgiler())
+                    tictactoe()
+                else:
+                    exit()
+tictactoe().oyun()
