83244e4f27040e27f876d8e83c2e539ebdbc0a67
Efe <efekocak070@gmail.com>
2023-06-29 00:24:45 +0300

    Update pTicTacToe.py

--- pTicTacToe.py
+++ pTicTacToe.py
@@ -1,79 +1,72 @@
 import random
-import os
-sayilar = []
-cizgiler = []
-def ekle(dene = ""):
-    if dene == "sil":
-        cizgiler.clear()
-        sayilar.clear()
+import os 
+class oyuncular():
+    def __init__(self,oyuncu1 = "",oyuncu2 = ""):
+        self.bir = oyuncu1
+        self.iki = oyuncu2   
+    def random_oyuncu(self):
+        self.liste = [
+            self.bir,
+            self.iki
+        ]
+        return self.liste
+def yaz(arg):
+    sayilar = []
     for u in range(1,10):
         sayilar.append(u)
-        cizgiler.append("-")
-def yazdir_cizgiler():
-    ekle()
-    return f"{cizgiler[0:3]}   {sayilar[0:3]}\n{cizgiler[3:6]}   {sayilar[3:6]}\n{cizgiler[6:9]}   {sayilar[6:9]}"
-os.system("cls")
-print(yazdir_cizgiler())
-class tictactoe:
-    def yatay_kontrol(arg):
-        if (cizgiler[0] == arg and cizgiler[1] == arg and cizgiler[2] == arg) or (cizgiler[3] == arg and cizgiler[4] == arg and cizgiler[5] == arg) or (cizgiler[6] == arg and cizgiler[7] == arg and cizgiler[8] == arg) or(cizgiler[0] == arg and cizgiler[3] == arg and cizgiler[6] == arg) or(cizgiler[1] == arg and cizgiler[4] == arg and cizgiler[7] == arg) or(cizgiler[2] == arg and cizgiler[5] == arg and cizgiler[8] == arg) or(cizgiler[0] == arg and cizgiler[4] == arg and cizgiler[8] == arg) or(cizgiler[2] == arg and cizgiler[4] == arg and cizgiler[6] == arg):
-            return True
+    os.system("cls")
+    print(f"{arg[0:3]}    {sayilar[0:3]}\n{arg[3:6]}    {sayilar[3:6]}\n{arg[6:9]}    {sayilar[6:9]}")
+class Tic_tac_toe():
     def __init__(self):
-        self.ilk_oyuncu = input("ilk oyuncunun isimi = ") #oyuncularin isimini aldik
-        self.ikinci_oyuncu = input("ikinci oyuncunun isimi = ")  
-        self.oyuncular = [self.ikinci_oyuncu,self.ilk_oyuncu]    
-        self.x_oyuncu = random.choice(self.oyuncular)
-        self.oyuncular.remove(self.x_oyuncu)
-        self.o_oyuncu = self.oyuncular[0] 
-        self.dogru = True
-    def oyun(self):
-        while self.dogru:
-            dogru = True
-            while dogru:
-                try:
-                    self.x_oyuncu_hamle = int(input(f"{self.x_oyuncu}(x) yer sec = "))
-                    dogru = False
-                except:
-                    dogru = True
-            while cizgiler[self.x_oyuncu_hamle-1] != "-":
-                self.x_oyuncu_hamle = int(input(f"{self.x_oyuncu} bos bir yer sec = "))
-            cizgiler[self.x_oyuncu_hamle-1] = "X"
-            sayilar[self.x_oyuncu_hamle-1] = 0
-            os.system("cls")
-            print(yazdir_cizgiler())
-            kontrol = tictactoe.yatay_kontrol("X")
-            if kontrol == True:
-                print(f"{self.x_oyuncu} oyunu kazandı")
-                evet = input("bir daha oynamak istermisiniz = ").lower()
-                if evet[0] == "e":
-                    ekle("sil")
-                    os.system("cls")
-                    print(yazdir_cizgiler())
-                    tictactoe().oyun()
-                else:
-                    exit()
-            dogru = True
-            while dogru:
-                try:
-                    self.o_oyuncu_hamle = int(input(f"{self.o_oyuncu}(O) yer secsin = "))
-                    dogru = False
-                except:
-                    dogru = True
-            while cizgiler[self.o_oyuncu_hamle-1] != "-":
-                self.o_oyuncu_hamle = int(input(f"{self.o_oyuncu} bos bir yer sec = "))
-            cizgiler[self.o_oyuncu_hamle-1] = "O"
-            sayilar[self.o_oyuncu_hamle-1] = 0
-            os.system("cls")
-            print(yazdir_cizgiler())
-            kontrol = tictactoe.yatay_kontrol("O")
-            if kontrol == True:
-                print(f"{self.o_oyuncu} oyunu kazandı")
-                evet = input("bir daha oynamak istermisiniz = ").lower()
-                if evet[0] == "e":
-                    ekle("sil")
-                    os.system("cls")
-                    print(yazdir_cizgiler())
-                    tictactoe().oyun()
-                else:
-                    exit()
-tictactoe().oyun()
+        os.system("cls")
+        self.cizgilerr = Tic_tac_toe.cizgiler()
+        self.oyuncular = Tic_tac_toe.random_oyuncu_sec()
+        self.x_oyuncusu = self.oyuncular[0]
+        self.y_oyuncusu = self.oyuncular[1]
+        self.cizgilere_yerles()
+    def cizgiler():
+        cizgilerlist = []
+        for u in range(9):
+            cizgilerlist.append("-")
+        return cizgilerlist
+    def random_oyuncu_sec():
+        birinci_oyuncu_isim = input("Oyuncu isimi:")
+        ikinci_oyuncu_isim = input("ikinci oyuncu isimi:")
+        oyuncular_liste = oyuncular(birinci_oyuncu_isim,ikinci_oyuncu_isim).random_oyuncu()
+        oyunculari = []
+        for u in range(len(oyuncular_liste)):
+            a = random.choice(oyuncular_liste)
+            oyunculari.append(a)
+            oyuncular_liste.remove(a)
+        return oyunculari
+    def cizgilere_yerles(self):
+        yaz(self.cizgilerr)
+        self.deneg = True
+        while self.deneg:
+            self.x_yer = int(input(f"(x) {self.x_oyuncusu} lutfen yerinizi seciniz:"))
+            while self.cizgilerr[self.x_yer-1] != "-":
+                self.x_yer = int(input(f"(x) {self.x_oyuncusu} lutfen yerinizi seciniz:"))
+            else:
+                self.cizgilerr[self.x_yer-1] = "X"
+            yaz(self.cizgilerr)
+            self.kontrol("X")
+            self.y_yer = int(input(f"(O) {self.y_oyuncusu} lutfen yerinizi seciniz:"))
+            while self.cizgilerr[self.y_yer-1] != "-":
+                self.y_yer = int(input(f"(O) {self.y_oyuncusu} lutfen yerinizi seciniz:"))
+            else:
+                self.cizgilerr[self.y_yer-1] = "O"
+            self.kontrol("O")
+            yaz(self.cizgilerr)
+    def kontrol(self,arg):
+        if (self.cizgilerr[0] == arg and self.cizgilerr[1] == arg and self.cizgilerr[2] == arg) or (self.cizgilerr[3] == arg and self.cizgilerr[4] == arg and self.cizgilerr[5] == arg) or (self.cizgilerr[6] == arg and self.cizgilerr[7] == arg and self.cizgilerr[8] == arg) or(self.cizgilerr[0] == arg and self.cizgilerr[3] == arg and self.cizgilerr[6] == arg) or(self.cizgilerr[1] == arg and self.cizgilerr[4] == arg and self.cizgilerr[7] == arg) or(self.cizgilerr[2] == arg and self.cizgilerr[5] == arg and self.cizgilerr[8] == arg) or(self.cizgilerr[0] == arg and self.cizgilerr[4] == arg and self.cizgilerr[8] == arg) or(self.cizgilerr[2] == arg and self.cizgilerr[4] == arg and self.cizgilerr[6] == arg):
+            if arg == "O":
+                kazanan = self.y_oyuncusu
+            else:
+                kazanan = self.x_oyuncusu
+            print(f"({arg}) {kazanan} oyuncusu kazandi")
+            evet = input("Bir daha oynamak istermisiniz E/H")
+            if evet[0].lower() == "e":
+                Tic_tac_toe()
+            else:
+                exit()
+Tic_tac_toe()
